c6b03a2425ca6b1966d47d703538b6c34b8ef1e7
Samattah <sammyattah1992@gmail.com>
2023-08-11 02:52:29 -0700

    4-inputs.c

--- 0x00-hello_world/4-inputs.c
+++ 0x00-hello_world/4-inputs.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    printf("Programming is like building a multilingual puzzle,\n");
-    return 0;
+/**
+ * main - Entry point of the program
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
