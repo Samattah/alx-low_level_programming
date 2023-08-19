a68f084846d9d9ccf3a8a7c9a04734e8f967d258
Samattah <sammyattah1992@gmail.com>
2023-08-19 05:39:47 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for printing combinations of two digits
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0 ; a <= 8 ; a++)
+	{
+		for (b = a + 1 ; b <= 9 ; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+				if (a != 8 || b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
