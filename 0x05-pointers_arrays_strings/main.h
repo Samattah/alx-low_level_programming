8c382064ee9e5315e74ef9963b8dac934fa130cf
Samattah <sammyattah1992@gmail.com>
2023-08-21 21:58:03 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,5 +16,6 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _putchar(char c);
+
 
 #endif
